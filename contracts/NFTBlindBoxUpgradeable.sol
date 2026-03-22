14bad0fb6d5eb6d379bb56d004ff6f766deb1f0c
moliyu <951028382@qq.com>
2026-03-22 16:07:42 +0800

    feat: nft

---
+++ contracts/NFTBlindBoxUpgradeable.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+
+contract NFTBlindBoxUpgradeable is Initializable, ERC721Upgradeable {}
